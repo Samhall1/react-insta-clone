11c28a728a73c912f20aed105ea91cc409fce40a
Sam <sam_rm@icloud.com>
2020-09-02 12:17:22 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
 import './App.css';
 
-function App() {
-  return (
-   <h1>Insta clone</h1>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <h1>Insta clone</h1>
+     );
+  }
+ 
 }
 
 export default App;

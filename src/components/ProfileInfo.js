14f7020ef2345b8aeee6e36c08fbe1ea51183a04
Silvia Petruta <silvia_petruta@yahoo.com>
2020-09-03 14:35:02 +0100

    Created the mainWindow for homePage and created the instaStories component, created commentPost, create the like button, added CSS for all the new components

---
+++ src/components/ProfileInfo.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BiDotsHorizontalRounded } from "react-icons/bi";
+import { AiOutlineHeart } from "react-icons/ai";
+import { FaRegComment } from "react-icons/fa";
+import { GrWaypoint } from "react-icons/gr";
+import { BsBookmark } from "react-icons/bs";
+import './ProfileInfo.css';
+
+
+const ProfileInfo = (props) => {
+    return (
+        <div className='profileInfo'>
+            <div className='userDetails'>
+                <img src={props.imageUser}/>
+                <div className='userNameAndLocation'>
+                    <p>{props.userName}</p>
+                    <p>{props.location}</p>
+                </div>
+                <BiDotsHorizontalRounded />
+            </div>
+            <img className="imagePost" src={props.imagePost}/>
+            <div className="profileIcons">
+                <div className="icons">
+                    <AiOutlineHeart onClick={props.likeButtonHandle}/>
+                    <FaRegComment />
+                    <GrWaypoint />
+                </div>
+                <BsBookmark />
+            </div>
+            <div className='postLikes'>
+                <p>Liked by <span>{props.firstPerson}</span> and <span>{props.allLikes}</span></p>
+            </div>
+            <div className='postComments'>
+                <p>
+                    <span>{props.userNameWhoLeftTheComment}</span> &nbsp; {props.randomComment}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileInfo;
